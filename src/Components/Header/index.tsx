e98764e5715f67b5dc4217117b2ae060ceb78e56
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2019-03-31 12:28:48 +0300

    Add responsive design to header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -17,56 +17,60 @@ export class Header extends React.Component<IHeader.IProps, IHeader.IState> {
 		const { pageType } = this.props;
 
 		return (
-			<nav className="navbar navbar-expand-lg sticky-top header">
+			<nav className="navbar navbar-expand-md sticky-top header navbar-light">
 				<div className="container">
-					{/* 	<a className="navbar-brand" href="#"> */}
 					<div className="header__logo">
 						<img src="/static/image/logo.png" alt=""></img>
 					</div>
-					{/* 		</a> */}
 					<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 						<span className="navbar-toggler-icon"></span>
 					</button>
-
 					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-				{/* 		<div className="test">
-							<div>1</div>
-							<div>2</div>
-							<div>3</div>
-							<div>4</div>
-
-						</div> */}
-									<ul className="navbar-nav  ">
+						<ul className="navbar-nav">
 							<li className={`nav-item ${pageType === 'home' ? 'active' : ''}`}>
 								<Link href="/home" as="/" >
-									<a className="nav-link">Anasayfa</a>
+									<a className="nav-link">Contact</a>
 								</Link>
 							</li>
 
 							<li className={`nav-item ${pageType === 'about' ? 'active' : ''}`}>
 								<Link href="/about" as="/about" prefetch>
-									<a className="nav-link">Hakkimizda</a>
+									<a className="nav-link">Advantages</a>
 								</Link>
 							</li>
 
 							<li className={`nav-item dropdown ${pageType === 'products' ? 'active' : ''}`}>
 								<Link href="/products" as="/products">
-									<a className="nav-link">Urunler</a>
+									<a className="nav-link">Services</a>
 								</Link>
 
 							</li>
 							<li className={`nav-item ${pageType === 'services' ? 'active' : ''}`}>
 								<Link href="/services" as="/services" prefetch>
-									<a className="nav-link">Hizmetler</a>
+									<a className="nav-link">Expertise</a>
 								</Link>
 							</li>
 						</ul>
-
 					</div>
 					<ul className="navbar-lang">
-						<li>Turkce</li>
-						<li>Hollandaca</li>
-						<li>Ingilizce</li>
+						<li className={`lang-item`}>
+							<Link href="/home" as="/" >
+								<a className="nav-link">English</a>
+							</Link>
+						</li>
+
+						<li className={`lang-item`}>
+							<Link href="/home" as="/" >
+								<a className="nav-link">Nederlands</a>
+							</Link>
+						</li>
+
+						<li className={`lang-item`}>
+							<Link href="/home" as="/" >
+								<a className="nav-link">Turkce</a>
+							</Link>
+						</li>
+
 					</ul>
 				</div>
 			</nav >
