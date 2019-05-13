a630dc62fa881e866a352bf951892c6a9dc68e24
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2019-05-13 23:27:09 +0300

    add horizantal aligments & remove typewtiter

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,35 +1,46 @@
 //#region Global Imports
 import * as React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators, Dispatch } from 'redux';
 import TextLoop from "react-text-loop";
 import { i18n, withNamespaces } from '../../i18n';
+import ReactTypingEffect from 'react-typing-effect';
+import Typing from 'react-typing-animation';
+
+import Typewriter from 'typewriter-effect';
 //#endregion Global Imports
 
 import './style.scss';
 //#region Interface Imports
 import { IHomePage, IStore } from '@Interfaces';
-import { HomeActions } from '@Actions';
 import { Layout } from '@Components';
 //#endregion Interface Imports
 
 export class HomePage extends React.Component<IHomePage.IProps, IHomePage.IState> {
 	public render(): JSX.Element {
-		const { t, i18n } = this.props;
+		const { i18n } = this.props;
+
 		return (
 			<Layout pageType={'home'}>
 				<div className="container-fluid home">
 					<div className="home__title">
 						{i18n.language === 'en' &&
 							<>
-								<p>Your &nbsp;
-								  <TextLoop interval={[3000, 2000]}>
-										<span>bridge</span>
-										<span>connection</span>
-										<span>home</span>
-										<span>harbor</span>
-									</TextLoop>{""}
-								</p>
+								<div className="home__typewriter">
+									<p>Your</p>
+								<span>This line will stay.</span>
+								<span>This line will get instantly removed after a 500 ms delay</span>
+
+							{/* 	<Typewriter
+									options={{
+										strings: ['bridge', 'connection', 'home', 'harbor'],
+										autoStart: true,
+										loop: true,
+									}}
+								/> */}
+									{/* <ReactTypingEffect
+										text={['bridge', 'connection', 'home', 'harbor']}
+										speed={120}
+									/> */}
+								</div>
 								<p>between </p>
 								<p>Belgium </p>
 								<p>and Turkey</p>
@@ -37,14 +48,13 @@ export class HomePage extends React.Component<IHomePage.IProps, IHomePage.IState
 						}
 						{i18n.language === 'nl' &&
 							<>
-								<p>Uw &nbsp;
-								  <TextLoop interval={[3000, 2000]}>
-										<span>brug</span>
-										<span>verbinding</span>
-										<span>thuis</span>
-										<span>haven</span>
-									</TextLoop>{""}
-								</p>
+								<div className="home__typewriter">
+									<p>Uw</p>
+									{/* <ReactTypingEffect
+										text={['brug', 'verbinding', 'thuis', 'haven']}
+										speed={120}
+									/> */}
+								</div>
 								<p>tussen</p>
 								<p>België</p>
 								<p>en Turkije.</p>
@@ -56,21 +66,25 @@ export class HomePage extends React.Component<IHomePage.IProps, IHomePage.IState
 								<p>ve Türkiye</p>
 								<p>arasindaki</p>
 								<p>
-									<TextLoop interval={[3000, 2000]}>
+									{/* <ReactTypingEffect
+										text={['köprünüz', 'bağlantınız', 'eviniz', 'limanınız']}
+										speed={120}
+									/> */}
+									{/* 		<TextLoop interval={[3000, 2000]}>
 										<span>köprünüz</span>
 										<span>bağlantınız</span>
 										<span>eviniz</span>
 										<span>limanınız</span>
-									</TextLoop>{""}.
+									</TextLoop>{""}. */}
 								</p>
 							</>
 						}
 					</div>
 					<div className="home__video">
-						<iframe className="youtube" 
+						<iframe className="youtube"
 							src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1">
 						</iframe>
-					{/* 	<div className="embed-responsive embed-responsive-16by9">
+						{/* 	<div className="embed-responsive embed-responsive-16by9">
 							<iframe className="embed-responsive-item" src="https://www.youtube.com/embed/tgbNymZ7vqY" allowFullScreen></iframe>
 						</div> */}
 					</div>
@@ -79,6 +93,5 @@ export class HomePage extends React.Component<IHomePage.IProps, IHomePage.IState
 		);
 	}
 }
-
 
 export default withNamespaces('common')(HomePage);
